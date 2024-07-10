0d9c48a06d875e496a1551360b87bc5e2af49ac3
dkssudgb <115577097+dkssudgb@users.noreply.github.com>
2024-07-10 15:33:24 +0900

    Initial commit

---
+++ bknd_class_01/src/main/java/kr/ac/cu/gbict/bknd_class_01/BkndClass01Application.java
@@ -0,0 +1,13 @@
+package kr.ac.cu.gbict.bknd_class_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkndClass01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkndClass01Application.class, args);
+	}
+
+}
---
+++ bknd_class_01/src/test/java/kr/ac/cu/gbict/bknd_class_01/BkndClass01ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.cu.gbict.bknd_class_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkndClass01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
