558f5c0fab58dd40d0c914ed60791adfca86ab7e
dkssudgb <115577097+dkssudgb@users.noreply.github.com>
2024-07-12 15:12:29 +0900

    bknd_class_03
    Updated

---
+++ bknd_class_03/src/main/java/kr/ac/cu/gbict/bknd_class_03/BkndClass03Application.java
@@ -0,0 +1,13 @@
+package kr.ac.cu.gbict.bknd_class_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkndClass03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkndClass03Application.class, args);
+	}
+
+}
---
+++ bknd_class_03/src/test/java/kr/ac/cu/gbict/bknd_class_03/BkndClass03ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.cu.gbict.bknd_class_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkndClass03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
